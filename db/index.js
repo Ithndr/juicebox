aa9a44eff4e0e787b02fed8e84a06cf6479ea9a8
Ithndr <tm21999@gmail.com>
2023-01-11 11:53:11 -0500

    completed part 1

--- db/index.js
+++ db/index.js
@@ -6,3 +6,38 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 module.exports = {
   client,
 }
+
+async function getAllUsers() {
+    try{
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+
+    return rows;
+    }catch(error){
+        console.error(error);
+    }
+  }
+
+  async function createUser({ username, password }) {
+    try {
+      const result = await client.query(`
+        INSERT INTO users(username, password) 
+        VALUES($1, $2) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `, [username, password]);
+  
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  // and export them
+  module.exports = {
+    client,
+    getAllUsers,
+    createUser,
+  }
--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,87 @@
-const { client } = require('./index');
+const {
+  client,
+  getAllUsers // new
+} = require('./index');
+
+const { 
+  // other imports,
+  createUser
+} = require('./index');
+
+// new function, should attempt to create a few users
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+
+    const albert = await createUser({ username: 'albert', password: 'bertie99' });
+    const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' });
+
+    console.log(albert);
+
+    console.log("Finished creating users!");
+  } catch(error) {
+    console.error("Error creating users!");
+    throw error;
+  }
+}
 
 async function testDB() {
-    try {
-      client.connect();
-  
-      const { rows } = await client.query(`SELECT * FROM users;`);
-      console.log(rows);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
+  try {
+
+    const users = await getAllUsers();
+    console.log(users);
+  } catch (error) {
+    console.error(error);
+  }
 }
-testDB();
+
+async function dropTables() {
+  try {
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+    `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+const run = async () => {
+  try {
+    client.connect();
+    console.log("rebuild here");
+    await rebuildDB();
+    console.log("test here");
+    await testDB();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+run();
